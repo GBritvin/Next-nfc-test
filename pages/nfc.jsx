aefbece44524c5ba0470b107382964612c988840
Grigory Britvin <gbritvin@siltech.ru>
2023-02-11 21:04:10 +0300

    set data

--- pages/nfc.jsx
+++ pages/nfc.jsx
@@ -14,6 +14,7 @@ export default function Nfc() {
             console.log("Record type:  " + record.recordType);
             console.log("MIME type:    " + record.mediaType);
             console.log("=== data ===\n" + decoder.decode(record.data));
+            setReadnik(decoder.decode(record.data));
           }
         };
       } catch (error) {
